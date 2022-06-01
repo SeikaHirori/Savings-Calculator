ee0830eea569b6190a55cfe76ddbeed813d0ce31
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-06-01 07:42:55 -0700

    Updated verison
    Attempted to pass TMC, but failed.
    
    Unsure of room of error, despite obtaining the desired outcome. The bare-bones UI passed when no data was input, so trying to figure out TMC's desired outcome has been confusing.
    
    Actual changes:
    
    - Fixed the calculations for both savings and interest amount.
    - Change the yAxis to not have any parameters; this allows the yAxis to dynamically scale according to the dataSet. For this context, this removes the need to make a method to account for the upper bound .

---
+++ application/SavingsCalculatorApplication.java
@@ -0,0 +1,110 @@
+package application;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.layout.BorderPane; // Foundation piece
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Slider;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SavingsCalculatorApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(SavingsCalculatorApplication.class, args);
+        System.out.println("Hello world!");
+
+        System.out.println("=========");
+
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        // Create BorderPane, the foundation layout
+        BorderPane layout = new BorderPane();
+
+        // Create Sliders
+        SubviewSlider savings = new SubviewSlider("Monthly savings", 25, 250, 25);
+        savings.adjustMajorTickUnit((double) 25f);
+        savings.adjustBlockIncrement((double) 5f);
+
+        SubviewSlider interest = new SubviewSlider("Yearly interest rate", 0, 10, 0);
+        interest.adjustBlockIncrement((double) .1f);
+        
+        SavingsGraph lineChart = new SavingsGraph("Savings calculator", savings, interest);
+
+        // Add sliders to VBox
+        VBox sliderLayout = new VBox();
+        sliderLayout.getChildren().addAll(savings.getBorderPane(), interest.getBorderPane());
+
+
+        // Add sub layouts
+        layout.setCenter(lineChart.getLineChart());
+        layout.setTop(sliderLayout);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        
+        //misc for TMC testing
+        Slider sliderSaving = savings.getSlider();
+        Slider sliderInterest = interest.getSlider();
+
+        // events
+        savings.getSlider().valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (!savings.getSlider().isValueChanging()) {
+                    layout.setCenter(lineChart.getLineChart());
+                    updateSliders(sliderSaving, sliderInterest, lineChart);
+                }
+            }
+            
+        });
+
+        interest.getSlider().valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (!interest.getSlider().isValueChanging()) {
+                    layout.setCenter(lineChart.getLineChart());
+                    updateSliders(sliderSaving, sliderInterest, lineChart);
+
+                }
+            }
+            
+        });
+
+
+
+        Scene view = new Scene(layout, 640, 480);
+
+
+        primaryStage.setScene(view);
+        primaryStage.show();
+    }
+
+    private static void updateSliders(Slider sliderSaving, Slider sliderInterest, SavingsGraph lineChart) {
+        sliderSaving = lineChart.getSavingsSlider();
+        sliderInterest = lineChart.getInterestSlider();
+    }
+    
+}
+
+
+
+
+
+/* Resources used:
+    - Resource #1: How to update label 'valuePosition' in realtime using ChangeListener.
+            - URL: https://docs.oracle.com/javafx/2/ui_controls/slider.htm
+        - Attempted to use ".setOnDrag", but it was inaccurate and did not update
+            in real time.
+
+    - Resource #2
+            - URL:https://stackoverflow.com/questions/68089808/javafx-slider-how-to-only-change-value-when-dragging-is-done-while-also-maintai
+
+*/
